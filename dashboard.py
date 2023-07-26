1a084f1f933054979e380b16b433008afc6fb96e
AbdelRahmanSousa <AbdelRahmanYousry81@gmail.com>
2023-07-26 20:43:13 +0400

    Test integration and adjust UI

--- dashboard.py
+++ dashboard.py
@@ -1,12 +1,13 @@
+import base64
 import os
 from subprocess import check_output
 from dash import Dash
 from dash.dependencies import Input, Output, State
-from FloorplanToBlender3d.FloorplanToBlenderLib import *
+from FloorplanToBlenderLib import *
 import dash_html_components as html
 import dash_core_components as cc
 import dash_bio
-
+from dash.exceptions import PreventUpdate
 
 # ---------------------------------For 3d reconstruction---------------------------------
 '''reconstructionModels = []
@@ -38,23 +39,34 @@ app.layout = html.Div([html.H1('Upload Plan'),
                        cc.Dropdown(options=[x[1:] for x in const.SUPPORTED_BLENDER_FORMATS], value=None,
                                    placeholder='Export format', id='export_type'),
                        cc.Upload(uploadDiv, id='plan'),
-                       html.Button('Download', id='download_btn', n_clicks=0)
+                       html.Button('Download', id='download_btn'),
+                       cc.Download(id="download")
                        ])
 
 
 # --------------------------Callbacks--------------------------
-@app.callback(Input('download_btn', 'n_clicks'), State('plan', 'contents'), State('plan', 'filename'), State('export_type', 'value'))
-def convert_plan(uploaded_contents, out_format):
+@app.callback(Output('download', 'data'),
+              [Input('download_btn', 'n_clicks')],
+              [State('plan', 'contents'),
+               State('plan', 'filename'),
+               State('export_type', 'value')])
+def convert_plan(n_clicks, uploaded_contents, file_name, out_format):
+    if file_name is None or uploaded_contents is None or out_format is None:
+        raise PreventUpdate()
     # Create image file and floor plan config
-    image_name = ''
+    image_name = './Data/' + file_name
+    image_file = open(image_name, 'wb+')
+    image_file.write(base64.b64decode(uploaded_contents.split(',')[1]))
+    image_file.close()
     # Parse configuration from system.ini
-    blender_install_path = config.get('/Config/system.ini', 'SYSTEM', const.STR_BLENDER_INSTALL_PATH)
-    target_base = config.get('/Config/system.ini', 'SYSTEM', const.STR_OUTPUT_FOLDER) + "\\" + config.get('/Config/system.ini', 'SYSTEM', const.STR_OUTPUT_NAME)
+    blender_install_path = config.get('./Config/system.ini', 'SYSTEM', const.STR_BLENDER_INSTALL_PATH)
+    target_base = config.get('./Config/system.ini', 'SYSTEM', const.STR_OUTPUT_FOLDER) + config.get(
+        './Config/system.ini', 'SYSTEM', const.STR_OUTPUT_NAME)
     target_path = target_base + const.BASE_FORMAT
     target_path = IO.get_next_target_base_name(target_base, target_path) + const.BASE_FORMAT
-    program_path = config.get('/Config/system.ini', 'SYSTEM', const.STR_PROGRAM_PATH)
+    program_path = config.get('./Config/system.ini', 'SYSTEM', const.STR_PROGRAM_PATH)
     # Create floor plan & execute feature extractor
-    fp = floorplan.new_floorplan('/Config/default.ini')
+    fp = floorplan.new_floorplan('./Config/default.ini')
     fp.image_path = image_name
     data_paths = execution.simple_single(fp)
     # Execute Blender scripts
@@ -64,38 +76,39 @@ def convert_plan(uploaded_contents, out_format):
             "-noaudio",  # this is a dockerfile ubuntu hax fix
             "--background",
             "--python",
-            config.get('/Config/system.ini', 'SYSTEM', const.STR_BLENDER_SCRIPT_CREATE_PATH),
+            config.get('./Config/system.ini', 'SYSTEM', const.STR_BLENDER_SCRIPT_CREATE_PATH),
             program_path,  # Send this as parameter to script
             target_path,
         ]
-        + data_paths
+        + [data_paths]
     )
+    export_path = None
+    export_base = None
+    drive, s = os.path.splitdrive(target_path)
+    if not drive:
+        export_path = program_path + target_path
+        export_base = program_path + target_base
+    else:
+        export_path = target_path
+        export_base = target_base
     if out_format != 'blend':
         check_output(
             [
                 blender_install_path,
                 "--background",
-                target_path,
+                export_path,
                 "--python",
-                config.get('/Config/system.ini', 'SYSTEM', const.STR_BLENDER_SCRIPT_EXPORT_PATH),
-                target_path,
+                config.get('./Config/system.ini', 'SYSTEM', const.STR_BLENDER_SCRIPT_EXPORT_PATH),
+                export_path,
                 '.' + out_format,
-                target_base + out_format,
+                export_base + '.' + out_format,
             ]
         )
-    return None
-
-
-@app.callback(Output('', '2dModel'), Input('', '2dModel'))
-def model_uploaded():
-    pass
+    # Clean up
+    IO.clean_data_folder(const.BASE_PATH)
+    return cc.send_file(export_base + '.' + out_format)
 
 
 if __name__ == '__main__':
     app.run_server(port=4050)
-'''html.H1('Choose Model Converter'),
-                       cc.Dropdown(options=reconstructionModels,
-                                   value=None, placeholder='2D To 3D Converter',
-                                   id='model_name'),
-                       html.H1('Upload Models'),
-                       cc.Upload(uploadDiv, id='2dModel', multiple=True)'''
+
